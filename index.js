ec1d29fcfb12605c99d66d2140302b70d397e95f
Bogar Martinez <bogar.cm@icloud.com>
2023-05-22 21:03:17 -0500

    Uploaded Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name : "Bob Bobson",
+    streetAddress: "WW33+68Q Guantanamo Bay",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key]= value;
+    return newObj;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value; 
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key){
+    delete employee.key; 
+    const employeeNewName = {...employee[key]};
+    return employeeNewName;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    employee[key]= value;
+    return employee;
+}
